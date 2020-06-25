5907d5fb273f134b8d21240d7a409b1f73e0bb35
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-25 20:22:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(@brands)
-    else
-      BRANDS << brand
+      BRANDS << brand unless BRANDS.any? {|b| b == brand}
+        
+      end
   end
 end
 
